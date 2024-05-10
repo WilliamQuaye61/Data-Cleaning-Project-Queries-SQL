9b55cc43bcd8e2f35f1352d849dfe8b8929aa8cc
WilliamQuaye61 <wquaye61@gmail.com>
2024-05-10 14:28:08 -0700

    Update Club Membership Cleaning.sql

--- Club Membership Cleaning.sql
+++ Club Membership Cleaning.sql
@@ -1,109 +1,109 @@
-select * 
-from club_member_info$
+SELECT * 
+FROM club_member_info$
 
 -- CHECKING FOR DUPLICATES
 
-With Duplicate_Cte as
+WITH Duplicate_Cte AS
 (
-select full_name,age,full_address,membership_date, ROW_NUMBER() over (partition by full_name order by full_name) as ROWnum
-from club_member_info$
+SELECT full_name,age,full_address,membership_date, ROW_NUMBER() over (partition by full_name order by full_name) as ROWnum
+FROM club_member_info$
 )
 
-select * 
-from Duplicate_cte
-where Rownum >1
+SELECT * 
+FROM Duplicate_cte
+WHERE Rownum >1
 
 --TRIMMING FULL NAME TO REMOVE UNWANTED SPACES FROM FULL NAME
 
-update club_member_info$
-set full_name = TRIM(full_name) ;
+UPDATE club_member_info$
+SET full_name = TRIM(full_name) ;
 
 --SPLITTING NAMES INTO FIRST AND LAST NAME
 
-select full_name, SUBSTRING(full_name,1,CHARINDEX(' ',full_name)) first_name,
+SELECT full_name, SUBSTRING(full_name,1,CHARINDEX(' ',full_name)) first_name,
 substring(full_name,CHARINDEX(' ',full_name),LEN(full_name)) last_name
-from club_member_info$ ;
+FROM club_member_info$ ;
 
 --ADDING AND UPDATING FIRST NAME AND LAST NAME COLUMN
 
-alter table club_member_info$
-add First_name nvarchar(50);
+ALTER TABLE club_member_info$
+ADD First_name nvarchar(50);
 
-update club_member_info$
-set first_name =  Upper(SUBSTRING(first_name,1,1))+Lower(SUBSTRING(first_name,2,LEN(first_name)));
+UPDATE club_member_info$
+SET first_name =  Upper(SUBSTRING(first_name,1,1))+Lower(SUBSTRING(first_name,2,LEN(first_name)));
 
-alter table club_member_info$
-add Last_name nvarchar(50);
+ALTER TABLE club_member_info$
+ADD Last_name nvarchar(50);
 
 
-update club_member_info$
-set Last_name = substring(full_name,CHARINDEX(' ',full_name),LEN(full_name));
+UPDATE club_member_info$
+SET Last_name = substring(full_name,CHARINDEX(' ',full_name),LEN(full_name));
 
 --CHANNGING FIRST NAME AND LAST NAME INT0 PROPER FORM
 
-select Replace(First_name,'?',' ')
-from club_member_info$
-where First_name like '?%'
+SELECT Replace(First_name,'?',' ')
+FROM club_member_info$
+WHERE First_name LIKE '?%'
 
-select First_name, Upper(SUBSTRING(first_name,1,1))+Lower(SUBSTRING(first_name,2,LEN(first_name)))
-from club_member_info$;
+SELECT First_name, Upper(SUBSTRING(first_name,1,1))+Lower(SUBSTRING(first_name,2,LEN(first_name)))
+FROM club_member_info$;
 
-update club_member_info$
-set first_name =  Upper(SUBSTRING(first_name,1,1))+Lower(SUBSTRING(first_name,2,LEN(first_name)));
+UPDATE club_member_info$
+SET first_name =  Upper(SUBSTRING(first_name,1,1))+Lower(SUBSTRING(first_name,2,LEN(first_name)));
 
-select Last_name, Upper(SUBSTRING(Last_name,CHARINDEX(' ',last_name),1))+Lower(SUBSTRING(Last_name,2,LEN(Last_name)))
-from club_member_info$;
+SELECT Last_name, Upper(SUBSTRING(Last_name,CHARINDEX(' ',last_name),1))+Lower(SUBSTRING(Last_name,2,LEN(Last_name)))
+FROM club_member_info$;
 
-update club_member_info$
-set Last_name = TRIM(Upper(SUBSTRING(Last_name,CHARINDEX(' ',last_name),1))+Lower(SUBSTRING(Last_name,2,LEN(Last_name))));
+UPDATE club_member_info$
+SET Last_name = TRIM(Upper(SUBSTRING(Last_name,CHARINDEX(' ',last_name),1))+Lower(SUBSTRING(Last_name,2,LEN(Last_name))));
 
 
 -- ADDING PROPER FULL NAME COLUMN
 
-alter table club_member_info$
-add Clean_full_Name nvarchar(50);
+ALTER TABLE club_member_info$
+ADD Clean_full_Name nvarchar(50);
 
-update club_member_info$
-set clean_full_name = First_Name + ' ' + Last_name;
+UPDATE club_member_info$
+SET clean_full_name = First_Name + ' ' + Last_name;
 
 --ADDING ADDRESS STREET, CITY AND STATE
 
 
-alter table club_member_info$
+ALTER TABLE club_member_info$
 add Address_Street nvarchar(50);
 
-alter table club_member_info$
-add Address_City nvarchar(50);
+ALTER TABLE club_member_info$
+ADD Address_City nvarchar(50);
 
-alter table club_member_info$
-add Address_State nvarchar(50);
+ALTER TABLE club_member_info$
+ADD Address_State nvarchar(50);
 
 
-select PARSENAME(Replace(full_address,',','.'),3),
+SELECT PARSENAME(Replace(full_address,',','.'),3),
          PARSENAME(Replace(full_address,',','.'),2),
 		 PARSENAME(Replace(full_address,',','.'),1)
-from club_member_info$;
+FROM club_member_info$;
 
 --UPDATING ADDRESS STREET,STATE AND CITY
 
-update club_member_info$
-set address_street = PARSENAME(Replace(full_address,',','.'),3);
+UPDATE club_member_info$
+SET address_street = PARSENAME(Replace(full_address,',','.'),3);
 
-update club_member_info$
-set address_city = PARSENAME(Replace(full_address,',','.'),2);
+UPDATE club_member_info$
+SET address_city = PARSENAME(Replace(full_address,',','.'),2);
 
-update club_member_info$
-set address_state = PARSENAME(Replace(full_address,',','.'),1);
+UPDATE club_member_info$
+SET address_state = PARSENAME(Replace(full_address,',','.'),1);
 
-select Clean_full_Name, REPLACE(Clean_full_Name,'?',' ')
-from club_member_info$
+SELECT Clean_full_Name, REPLACE(Clean_full_Name,'?',' ')
+FROM club_member_info$
 
 --UPDATING MARITAL STATUS INTO PROPER FORM
-Select Marital_Status,Upper(SUBSTRING(Marital_Status,1,1))+ Lower(SUBSTRING(Marital_Status,2,LEN(Marital_Status)))
-from club_member_info$
+SELECT Marital_Status,Upper(SUBSTRING(Marital_Status,1,1))+ Lower(SUBSTRING(Marital_Status,2,LEN(Marital_Status)))
+FROM club_member_info$
 
-update club_member_info$
-set Marital_Status = Upper(SUBSTRING(Marital_Status,1,1))+ Lower(SUBSTRING(Marital_Status,2,LEN(Marital_Status)))
+UPDATE club_member_info$
+SET Marital_Status = Upper(SUBSTRING(Marital_Status,1,1))+ Lower(SUBSTRING(Marital_Status,2,LEN(Marital_Status)))
 
 --RENAMING COLUMN NAMES
 
